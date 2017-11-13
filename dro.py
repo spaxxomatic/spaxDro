b370768689871d211de26287fd32951bf2ef730e
Lucian Nutiu <lucian.nutiu@iso-gruppe.com>
2017-11-13 17:26:54 +0100

    adding macro funcs

--- dro.py
+++ dro.py
@@ -4,8 +4,9 @@ import serial
 import ConfigParser
 from thread import start_new_thread
 import time
+import dromacros as macros
 #from PIL import ImageTk, Image
-
+configfile = "dro.ini"
 root = Tk()
 xdisplay = StringVar()
 ydisplay = StringVar()
@@ -14,9 +15,16 @@ wdisplay = StringVar()
 bdisplay = StringVar()
 rpmdisplay = StringVar()
 debugstr = StringVar()
-xstep = float(15)  #micron/step
-ystep = float(15)  #micron/step
-zstep = float(15)  #micron/step
+xstep, ystep, zstep = None, None, None
+config = ConfigParser.ConfigParser()
+config.read(configfile)
+try:
+    xstep = float(config.get('RESOLUTION', 'x_step_size'))
+    ystep = float(config.get('RESOLUTION', 'y_step_size'))
+    zstep = float(config.get('RESOLUTION', 'z_step_size'))
+except Exception, e:
+    print "Invalid config file: %s"%(str(e))
+    exit(1)
 
 class Storage(list): pass
 class Display:pass #forward declaration
@@ -24,7 +32,6 @@ class Display:pass #forward declaration
 def on_closing():
     if tkMessageBox.askokcancel("Quit", "Do you want to quit?"):
         Display.exit = 1
-        
         root.destroy()
 root.protocol("WM_DELETE_WINDOW", on_closing)
 
@@ -47,9 +54,9 @@ class Display():
             zdisplay.set( self.globalerr)
         else:    
             #xdisplay.set(xval)
-            xdisplay.set("X: %.3f"%(xstep*(self.xval - self.xval_corr) / 1000))
-            ydisplay.set("Y: %.3f"%(ystep*(self.yval - self.yval_corr)/ 1000))
-            zdisplay.set("Z: %.3f"%(zstep*(self.zval - self.zval_corr)/ 1000))
+            xdisplay.set("%.4f"%(xstep*(self.xval - self.xval_corr) / 1000))
+            ydisplay.set("%.4f"%(ystep*(self.yval - self.yval_corr)/ 1000))
+            zdisplay.set("%.4f"%(zstep*(self.zval - self.zval_corr)/ 1000))
                 
     def history(self):
         self.xval_corr = self.xstorage.pull()
@@ -80,15 +87,15 @@ SENSOR_ERRORS = {
 class SerialConn():
     buff = []
     def __init__(self, port, display):
-		self.con = serial.Serial(
-			port=port, \
-			baudrate=57600, \
-			parity=serial.PARITY_NONE, \
-			stopbits=serial.STOPBITS_ONE, \
-			bytesize=serial.EIGHTBITS, \
-			timeout=None)
-		self.display = display
-		
+        self.con = serial.Serial(
+            port=port, \
+            baudrate=57600, \
+            parity=serial.PARITY_NONE, \
+            stopbits=serial.STOPBITS_ONE, \
+            bytesize=serial.EIGHTBITS, \
+            timeout=None)
+        self.display = display
+
     def receive(self):
         xdisplay.set( '..init..')
         while True:
@@ -124,7 +131,7 @@ class SerialConnMock(SerialConn):
             display.yval = self.yval
             display.zval = self.zval
             #print " --------------%i "%self.xval
-            time.sleep(0.1)
+            time.sleep(0.02)
             
                     
 display = Display()
@@ -146,10 +153,6 @@ class Settings():
         #l = Label(self.window, text="This is window" )
         #l.pack(side="top", fill="both", expand=True, padx=100, pady=100)
 
-class DroFunctions:
-    def drillseries(self):
-        pass
-
     
 class Application(Frame):
     def settings(self):
@@ -161,74 +164,111 @@ class Application(Frame):
     def update_pos(self):
         self.chx += 1
         #Button(self.HISTORYFRAME_X, text='QUIT', fg='red', command=on_closing).grid(row=0,column=self.chx)
-        self.master.after(50, self.update_pos)
+        self.master.after(30, self.update_pos)
         self.pos_display.refresh()
+    def nextrow(self):
+        self._thisrow += 1
+        return self._thisrow
+    
+    def place_next(self, obj):
+        obj.grid(row=self.nextrow())
+        return obj
+    
+    def create_axis_digitdisplay(self, axis, parent, pos_variable):
+        #setattr(self, '%POSFRAME'
+        f = Frame(parent)
+        Label(f, text=axis, bg='#efe', relief=FLAT, height=1, width=30, font = "Calibri 32").pack({"side": "left"})
+        Button(f, text="Zero", command=display.zeroX).pack({"side": "right"})
+        Button(f, text="Abs/Rel", command=display.saveX).pack({"side": "right"})
+        Label(f, textvariable=pos_variable, bg='#efe', relief=FLAT, height=1, width=30, font = "Arial 32 ").pack({"side": "left"})
+        f.pack()
         
+    
     def createWidgets(self):
         self.chx = 0
-        #self.QUIT = Button(self, text='QUIT', fg='red', command=on_closing).pack({"side": "left"})
-        #self.Settings = Button(self, text='Settings', fg='green', command=self.settings).pack({"side": "left"})
+        self.MENUFRAME = Frame(root, width=self.width, height=30)
+        
+        self.QUIT = Button(self.MENUFRAME, text='QUIT', fg='red', command=on_closing).grid(row=0, column=0)
+        self.Settings = Button(self.MENUFRAME, text='Settings', fg='green', command=self.settings).grid(row=0, column=1)
+        self.place_next(self.MENUFRAME)
         
         self.dbg = Label(root, textvariable=debugstr, relief=FLAT, height=1, width=70, font="Arial 10 bold")
-        self.dbg.grid(row=1)
-        self.HISTORYFRAME = Frame(root, width=768, height=30)
+        self.dbg.grid(row=self.nextrow())
+        self.HISTORYFRAME = Frame(root, width=self.width, height=30)
         
         Label(self.HISTORYFRAME, text="XMEM", bg='#efe',  height=1,  bd=1, width=12, font = "Calibri 12 bold").grid(row=0)
-        self.HISTORYFRAME_X = self.histframe().grid(row=0, column=1)
+        self.HISTORYFRAME_X = self.histframe().grid(row=self.nextrow(), column=1)
         
         Label(self.HISTORYFRAME, text="YMEM", bg='#efe',  height=1,  bd=1, width=12, font = "Calibri 12 bold").grid(row=1)
-        self.HISTORYFRAME_Y = self.histframe().grid(row=1, column=1)
+        self.HISTORYFRAME_Y = self.histframe().grid(row=self.nextrow(), column=1)
 
         Label(self.HISTORYFRAME, text="ZYMEM", bg='#efe',  height=1,  width=12, font = "Calibri 12 bold").grid(row=2)
-        self.HISTORYFRAME_Z = self.histframe().grid(row=2, column=1) 
+        self.HISTORYFRAME_Z = self.histframe().grid(row=self.nextrow(), column=1) 
         #Label(self.HISTORYFRAME_Z, text="ZMEM", bg='#efe',  height=1, width=12, font = "Calibri 12 bold").pack(side=LEFT)
+        self.place_next(self.HISTORYFRAME)
+        
+        self.DISPLAYFRAME = Frame(root, width=self.width)
+        self.place_next(self.DISPLAYFRAME)
+        
+        self.TOOLSFRAME = Frame(root, bg='#fff', width=self.width)
+        self.place_next(self.TOOLSFRAME)
+        
+        self.create_axis_digitdisplay('X', self.DISPLAYFRAME, xdisplay)
+        self.create_axis_digitdisplay('Y', self.DISPLAYFRAME, ydisplay)
+        self.create_axis_digitdisplay('Z', self.DISPLAYFRAME, zdisplay)
+        #self.XPOSFRAME = Frame(self.DISPLAYFRAME)
+        #self.XPOSFRAME.pack()
+        #self.XZERO = Button(self.XPOSFRAME, text="Zero", command=display.zeroX).pack({"side": "right"})
+        #self.XSTORE = Button(self.XPOSFRAME, text="Abs/Rel", command=display.saveX).pack({"side": "right"})
+        #self.XD = Label(self.XPOSFRAME, textvariable=xdisplay, bg='#efe', relief=FLAT, height=1, width=30, font = "Calibri 32 bold")
+        #self.XD.pack({"side": "left"})
 
-        self.HISTORYFRAME.grid(row=0)
-        
-        self.DISPLAYFRAME = Frame(root, width=768, height=576)
-        self.DISPLAYFRAME.grid(row=1)
-        
-        self.TOOLSFRAME = Frame(root, bg='#fff', width=400)
-        self.TOOLSFRAME.grid(row=2)
-        
-        self.XFRAME = Frame(self.DISPLAYFRAME)
-        self.XFRAME.pack()
-        self.XZERO = Button(self.XFRAME, text="Zero", command=display.zeroX).pack({"side": "right"})
-        self.XSTORE = Button(self.XFRAME, text="Abs/Rel", command=display.saveX).pack({"side": "right"})
-        self.XD = Label(self.XFRAME, textvariable=xdisplay, bg='#efe', relief=FLAT, height=1, width=30, font = "Calibri 32 bold")
-        self.XD.pack({"side": "left"})
-        #self.XZERO.pack({"side": "right"})
-        #self.XSTORE.pack({"side": "right"})
-
-        self.YFRAME = Frame(self.DISPLAYFRAME)
-        self.YFRAME.pack()
-        self.YZERO = Button(self.YFRAME, text="Zero", command=display.zeroY).pack({"side": "right"})
-        self.YSTORE = Button(self.YFRAME, text="Abs/Rel", command=display.saveY).pack({"side": "right"})
-        self.YD = Label(self.YFRAME, textvariable=ydisplay, bg='#efe', relief=FLAT, height=1, width=30, font = "Calibri 32 bold")
-        self.YD.pack({"side": "left"})
+        #self.YPOSFRAME = Frame(self.DISPLAYFRAME)
+        #self.YPOSFRAME.pack()
+        #self.YZERO = Button(self.YPOSFRAME, text="Zero", command=display.zeroY).pack({"side": "right"})
+        #self.YSTORE = Button(self.YPOSFRAME, text="Abs/Rel", command=display.saveY).pack({"side": "right"})
+        #self.YD = Label(self.YPOSFRAME, textvariable=ydisplay, bg='#efe', relief=FLAT, height=1, width=30, font = "Calibri 32 bold")
+        #self.YD.pack({"side": "left"})
         #self.YZERO.pack({"side": "right"})
         #self.YSTORE.pack({"side": "right"})
 
 
-        self.ZFRAME = Frame(self.DISPLAYFRAME)
-        self.ZFRAME.pack()
-        self.ZZERO = Button(self.ZFRAME, text="Zero", command=display.zeroZ).pack({"side": "right"})
-        self.ZSTORE = Button(self.ZFRAME, text="Abs/Rel", command=display.saveZ).pack({"side": "right"})
-        self.ZD = Label(self.ZFRAME, textvariable=zdisplay, bg='#efe', relief=FLAT, height=1, width=30, font = "Calibri 32 bold")
-        self.ZD.pack({"side": "left"})
+        #self.ZFRAME = Frame(self.DISPLAYFRAME)
+        #self.ZFRAME.pack()
+        #self.ZZERO = Button(self.ZFRAME, text="Zero", command=display.zeroZ).pack({"side": "right"})
+        #self.ZSTORE = Button(self.ZFRAME, text="Abs/Rel", command=display.saveZ).pack({"side": "right"})
+        #self.ZD = Label(self.ZFRAME, textvariable=zdisplay, bg='#efe', relief=FLAT, height=1, width=30, font = "Calibri 32 bold")
+        #self.ZD.pack({"side": "left"})
         #self.ZZERO.pack({"side": "right"})
 
         #self.TOOLSFRAME = Frame(root, bg='red',relief=FLAT, borderwidth=5)
         #self.TOOLSFRAME.pack(side=TOP)
-        self.drillseries_icon = PhotoImage(file="drill.gif")
-        b = Button(self.TOOLSFRAME, compound=LEFT, image=self.drillseries_icon, command=DroFunctions().drillseries)
+
+        canvas_height = 300
+        c = self.canvas = Canvas(root, width=self.width, height=canvas_height)
+        c.height = canvas_height
+        c.width = self.width
+        c.config(relief=GROOVE, bg="#aa9")
+        c.grid(row=self.nextrow())
+        macros.canvas = c
+        macros.root = root
+        self.drillseries_rect_icon = PhotoImage(file="rect_drills.gif")
+        self.drillseries_circ_icon = PhotoImage(file="circle_drills.gif")
+        b = Button(self.TOOLSFRAME, compound=LEFT, image=self.drillseries_rect_icon, command=macros.Funcs.drillseries_rect).pack({"side": "left"})
+        b = Button(self.TOOLSFRAME, compound=LEFT, image=self.drillseries_circ_icon, command=macros.Funcs.drillseries_circ).pack({"side": "left"})
+                
+        #c.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
+        #c.create_rectangle(50, 25, 150, 75, fill="blue")
+
         #b.grid(row=1, column=1, padx=130)#
-        b.pack({"side": "top"})
+        
         self.update_pos()
         
 
-    def __init__(self, master, pos_display):
+    def __init__(self, master, pos_display, width=1000, height=680):
         Frame.__init__(self, master)
+        self.width, self.height = width, height
+        self._thisrow = 0
         self.grid(row=2)
         self.pos_display = pos_display
         self.createWidgets()
@@ -239,14 +279,6 @@ app = Application(master=root, pos_display=display)
 title = "Spaxx DRO "
 app.master.title(title)
 app.master.minsize(1000, 800)
-config = ConfigParser.ConfigParser()
-config.read("dro.ini")
-try:
-    xstep = int(config.get('RESOLUTION', 'x_step_size'))
-    ystep = int(config.get('RESOLUTION', 'y_step_size'))
-    zstep = int(config.get('RESOLUTION', 'z_step_size'))
-except Exception:
-    raise "Invalid config file: %s"%(`e`)
 port = config.get('GENERAL', 'comport')
 mode = config.get('GENERAL', 'mode')
 #thread_display = start_new_thread(display.refresh, ())
@@ -268,5 +300,5 @@ try:
 except Exception, e:
         debugstr.set(`e`)
         display.globalerr = 'INIT ERR'
-
+        
 app.mainloop()
---
+++ dromacros.py
@@ -0,0 +1,161 @@
+from Tkinter import *
+import tkMessageBox
+import ConfigParser
+import math
+
+canvas = None
+root = None
+
+class Projector():
+    def __init__(self, canvas, keep_scale_ratio=True):
+        self.canvas = canvas
+        self.xpad = 10
+        self.ypad = 10
+        self.keep_scale_ratio = keep_scale_ratio
+        canvas.delete("all")
+    def show_points(self, coords, x_max, y_max, x_min=0, y_min=0):
+        radius = 6#self.screenwidth*self.screenheight/(len(coords)**2)
+        #print "diam %s"%diam
+        (xpad, ypad) = self.xpad , self.ypad
+        self.ypad = 10
+        self.canvas
+        x_max = x_max - x_min
+        y_max = y_max - y_min
+        x_size_factor = (self.canvas.width-2*xpad)/x_max
+        y_size_factor = (self.canvas.height-2*ypad)/y_max
+        if self.keep_scale_ratio:
+            if x_size_factor < y_size_factor:
+                y_size_factor = x_size_factor
+            else:
+                x_size_factor = y_size_factor
+        self.canvas.config(width=x_max*x_size_factor + 2*xpad, height=y_max*y_size_factor + 2*ypad)            
+        #print "factor %s"%size_factor
+        canvas_width, canvas_height = self.canvas.width, self.canvas.height
+        for idx, p in enumerate(coords):
+            x = (p[0]-x_min)*x_size_factor + xpad  #x_min -> shift to 0
+            y = (p[1]-y_min)*y_size_factor + ypad
+            self.canvas.create_line(0, y, canvas_width, y, fill="#ccc")
+            self.canvas.create_line(x, 0, x, canvas_height, fill="#ccc")
+            self.canvas.create_oval(x-radius, y-radius, x+radius, y+radius, fill='#55b')
+            
+class Mbox(object):
+
+    root = None
+
+    def __init__(self, msg, dict_key=None):
+        """
+        msg = <str> the message to be displayed
+        dict_key = <sequence> (dictionary, key) to associate with user input
+        (providing a sequence for dict_key creates an entry for user input)
+        """
+        tki = tkinter
+        self.top = tki.Toplevel(Mbox.root)
+
+        frm = tki.Frame(self.top, borderwidth=4, relief='ridge')
+        frm.pack(fill='both', expand=True)
+
+        label = tki.Label(frm, text=msg)
+        label.pack(padx=4, pady=4)
+
+        if caller_wants_an_entry:
+            self.entry = tki.Entry(frm)
+            self.entry.pack(pady=4)
+
+            b_submit = tki.Button(frm, text='Submit')
+            b_submit['command'] = lambda: self.entry_to_dict(dict_key)
+            b_submit.pack()
+
+        b_cancel = tki.Button(frm, text='Cancel')
+        b_cancel['command'] = self.top.destroy
+        b_cancel.pack(padx=4, pady=4)
+
+    def entry_to_dict(self, dict_key):
+        data = self.entry.get()
+        if data:
+            d, key = dict_key
+            d[key] = data
+            self.top.destroy()
+            
+class MacroDialog:
+    def __init__(self, parent, title, inputs, defvals=None):
+        top = self.top = Toplevel(parent)
+        Label(top, text=title).grid(row=0)
+        row = 0
+        self.inputboxes = []
+        self.returns = []
+        for d in inputs:
+            Label(top, text=d).grid(row=row, column=0)
+            b = Entry(top)
+            self.inputboxes.append(b)
+            if defvals:
+                b.insert(0,defvals[row])
+            #b_submit['command'] = lambda: self.entry_to_dict(dict_key)
+            b.grid(row=row, column=1)
+            row += 1  
+        
+        b = Button(top, text="OK", command=self.ok)
+        b.grid(row=5)
+        #top.update()
+    
+    def validate(self):
+        try:
+            for inp in self.inputboxes:
+                val = int(inp.get())
+                self.returns.append(val)
+            return 1
+        except ValueError:
+            tkMessageBox.showwarning(
+                "Bad input",
+                "Illegal values, no chars allowed"
+            )
+            return 0
+    def ok(self):
+        #self.input = (self.no_x.get(), self.spacing_x.get(),self.no_y.get(), self.spacing_y.get())
+        if self.validate():
+            self.top.destroy()
+
+class Funcs:
+
+    @classmethod
+    def drillseries_rect(self):
+        projector = Projector(canvas)
+        
+        #canvas.create_rectangle(50, 25, 150, 75, fill="blue")
+        #projector.show_points([(0,0), (0,22), (3,0), (3,24)], 3, 24)
+        inputs = ["X Number of holes", "X Spacing", "Y Number of holes", "Y Spacing"]
+        d = MacroDialog(root, 'Rectangular drill grid', inputs, defvals=[1,1,1,1])
+        root.wait_window(d.top)
+        ret = d.returns
+        def calculate_coords(x_no, x_space, y_no, y_space):
+            coords = []
+            for i in range(x_no):
+                for j in range(y_no):
+                    coords.append((x_space*i, y_space*j))
+            return coords, x_space*x_no, y_space*y_no,0,0
+        coords, x_max, y_max, x_min, y_min = calculate_coords(*ret)
+        projector.show_points(coords, x_max, y_max, x_min, y_min)
+
+    @classmethod
+    def drillseries_circ(self):    
+        projector = Projector(canvas)
+        inputs = ["Circle radius", "No of holes", "Start angle"]
+        d = MacroDialog(root, 'Circular drill grid', inputs, defvals=[20,4,0])
+        root.wait_window(d.top)
+        ret = d.returns
+        def calculate_coords(radius, no_holes, start_angle):
+            coords = []
+            x_max, y_max, x_min, y_min =  0,0,0,0
+            for i in range(no_holes):
+                angle_step = 360.0/no_holes
+                x,y = (radius*math.cos(math.radians(i*angle_step+start_angle)), 
+                        radius*math.sin(math.radians(i*angle_step+start_angle)))
+                coords.append((x,y))
+                if x < x_min: x_min = x
+                if y < y_min: y_min = y
+                if x > x_max: x_max = x
+                if y > y_max: y_max = y
+            return coords, x_max, y_max, x_min, y_min
+        #coords, x_max, y_max = calculate_coords(*ret)
+        projector.show_points(*calculate_coords(*ret))            
+        #canvas.create_rectangle(50, 25, 120, 75, fill="red")
+        
